805d96dafce712bba5aed90a52a9135c9db60231
dave <dave@computesoftware.com>
2018-07-28 10:22:56 -0700

    Find all unique clause "paths" through boolean expression tree. Need to handle negation.

--- src/compute/retescript/core.clj
+++ src/compute/retescript/core.clj
@@ -9,6 +9,24 @@
             [clojure.pprint :refer [pprint]])
   (:import (datascript.query Context)))
 
+(defn tree-seq-path
+  "Like core's tree-seq but returns a lazy sequence of vectors of the
+  paths of the nodes in a tree, via a depth-first walk. It optionally
+  applies node-fn to each node before adding it to the path. branch?
+  must be a fn of one arg that returns true if passed a node that can
+  have children (but may not).  children must be a fn of one arg that
+  returns a sequence of the children. Will only be called on nodes for
+  which branch? returns true. Root is the root node of the tree."
+  [branch? children root & [node-fn]]
+  (let [node-fn (or node-fn identity)
+        walk (fn walk [path node]
+               (let [new-path (conj path (node-fn node))]
+                 (lazy-seq
+                    (cons new-path
+                            (when (branch? node)
+                              (mapcat (partial walk new-path) (children node)))))))]
+    (walk [] root)))
+
 (defn replace-query-vars
   [x]
   (if (and (symbol? x) (= \? (first (str x))))
@@ -18,6 +36,18 @@
 (defn pattern?
   [node]
   (= datascript.parser.Pattern (type node)))
+
+(defn and?
+  [node]
+  (= datascript.parser.And (type node)))
+
+(defn not?
+  [node]
+  (= datascript.parser.Not (type node)))
+
+(defn or?
+  [node]
+  (= datascript.parser.Or (type node)))
 
 (defn function?
   [node]
@@ -95,45 +125,59 @@
       :vars        '~(conj (set args) binding-var)
       :fn          (fn [~@args] {'~binding-var ~body})}))
 
+(defn clause-paths
+  [negate? paths [clause & clauses]]
+  (if clause
+    (cond
+      (and? clause) (let [paths' (clause-paths negate? paths (:clauses clause))]
+                      (recur negate? paths' clauses))
+
+      (not? clause) (let [paths' (vec (mapcat #(clause-paths (not negate?) paths [%]) (:clauses clause)))]
+                      (recur negate? paths' clauses))
+
+      (or? clause) (let [paths' (vec (mapcat #(clause-paths negate? paths [%]) (:clauses clause)))]
+                     (recur negate? paths' clauses))
+
+      :else (let [paths' (mapv #(conj % (assoc clause :negate? negate?)) paths)]
+              (recur negate? paths' clauses)))
+    paths))
+
 (defn compile-query
   [query-def]
   (let [[name query] query-def
         query-ast (dp/parse-query query)
-        patterns (->> query-ast :qwhere (map (fn [p] `'~(unparse-pattern p))))]
+        paths (clause-paths false [[]] (:qwhere query-ast))
+        path-binders (->> paths
+                          (map #(->> %
+                                     (mapv (fn [p]
+                                             (-> (cond
+                                                   (pattern? p) (pattern-clause-binding-fn (unparse-pattern p))
+
+                                                   (function? p) (function-clause-binding-fn (unparse-function p) (->> p :args (map :symbol) (filter dq/free-var?) vec))
+
+                                                   (predicate? p) (predicate-clause-fn (unparse-predicate p) (->> p :args (map :symbol) (filter dq/free-var?) vec)))
+                                                 (assoc :negate? (:negate? p)))))))
+                          (mapv (fn [binders]
+                                  {:pattern-binders (->> binders (filter #(= :pattern (:type %))) vec)
+                                   :function-binders (->> binders (filter #(= :function (:type %))) vec)
+                                   :predicates (->> binders (filter #(= :predicate (:type %))) vec)
+                                   :bindings  {}})))]
     {:name      name
      :query     `'~query
      :query-ast query-ast
-     :bindings  {} #_(->> query-ast
-                          :qwhere
-                          (filter pattern?)
-                          (map (fn [p]
-                                 [`'~(unparse-pattern p)
-                                  `'{:vars     ~(->> p :pattern (filter variable?) (map :symbol) set)
-                                     :bindings #{}}]))
-                          (into {}))}))
+     :path-binders path-binders}))
 
 (defn compile-rule
   [rule-def]
   (let [[name query _ & rhs] rule-def
         cq (compile-query [name query])
-        binders (->> cq
-                     :query-ast
-                     :qwhere
-                     (mapv (fn [p]
-                             (cond
-                               (pattern? p) (pattern-clause-binding-fn (unparse-pattern p))
 
-                               (function? p) (function-clause-binding-fn (unparse-function p) (->> p :args (map :symbol) (filter dq/free-var?) vec))
-
-                               (predicate? p) (predicate-clause-fn (unparse-predicate p) (->> p :args (map :symbol) (filter dq/free-var?) vec))))))
 
         rhs-args (->> cq :query-ast :qfind :elements (mapv :symbol))
         rhs-fn `(fn [~@rhs-args]
                   ~@rhs)]
-    (assoc cq :pattern-binders (->> binders (filter #(= :pattern (:type %))) vec)
-              :function-binders (->> binders (filter #(= :function (:type %))) vec)
-              :predicates (->> binders (filter #(= :predicate (:type %))) vec)
-              :rhs-args `'~rhs-args
+
+    (assoc cq :rhs-args `'~rhs-args
               :rhs-fn rhs-fn)))
 
 (defmacro defrules
@@ -159,27 +203,45 @@
       =>
       (println ?e ?v)]
 
-   [::r3
-    [:find ?e ?x ?z
+   #_[::r3
+      [:find ?e ?x ?z
+       :where
+       [?e :a ?v]
+       [(+ ?x 2) ?z]
+       [(* ?v 0.3) ?x]]
+      =>
+      (println "X" ?x ?z)
+      [[:db/add ?e :x ?x]]]
+
+   #_[::r4
+      [:find ?e1 ?v2
+       :where
+       [?e1 :a _]
+       [_ :a ?v2]
+       #_[_ :b ?v2]
+       [(identity ?v2) ?q]
+       #_[(inc ?v2) ?q]
+       [?e1 :a ?q]]
+      =>
+      (println "R4" ?e1 ?v2)]
+
+   [:r5
+    [:find ?e ?v ?w ?q
      :where
      [?e :a ?v]
-     [(+ ?x 2) ?z]
-     [(* ?v 0.3) ?x]]
+     [?e :b ?w]
+     [?e :c ?q]
+     (not [?e :c 1]
+          [?e :d 2]
+          [(> ?w 5)]
+          #_[(identity ?e) ?e])
+     (or [?e :a 1]
+         (and [?e :a 2]
+              [?e :b 1]))]
     =>
-    (println "X" ?x ?z)
-    [[:db/add ?e :x ?x]]]
+    (println "R5" ?e ?v ?w ?q)
+    [[?e :foo :bar]]]])
 
-   [::r4
-    [:find ?e1 ?v2
-     :where
-     [?e1 :a _]
-     [_ :a ?v2]
-     #_[_ :b ?v2]
-     [(identity ?v2) ?q]
-     #_[(inc ?v2) ?q]
-     [?e1 :a ?q]]
-    =>
-    (println "R4" ?e1 ?v2)]])
 
 (defn join-graph-components
   [rule]
@@ -207,7 +269,7 @@
         g (-> (graph/digraph)
               (graph/add-nodes* function-binders)
               (graph/add-edges* edges))]
-    (alg/topsort g)))
+    (vec (alg/topsort g))))
 
 (defn group-join-patterns
   [rule]
@@ -229,12 +291,18 @@
         (assoc :joined-binders jbs)
         (dissoc :pattern-binders #_:function-binders))))
 
+(defn group-and-sort-path-binders
+  [path-binders]
+  (mapv (fn [binders]
+          (-> binders
+              (update :function-binders sort-function-binders)
+              group-join-patterns))
+        path-binders))
+
 (defn create-session
   [& rulesets]
-  {:rules           (vec (mapcat #(->> %
-                                       (map (fn [r] (update r :function-binders sort-function-binders)))
-                                       (map group-join-patterns))
-                                 rulesets))
+  {:rules           (vec (map #(update % :path-binders group-and-sort-path-binders)
+                              (apply concat rulesets)))
    :pending-tx-data #{}})
 
 (defn join-bindings
@@ -302,20 +370,20 @@
                                                           {:vars (-> bs keys set) :binding bs})])
                [#{} {:vars #{} :binding {}}])))
 
-(defn run-rule
-  [fact rule]
+(defn run-path-binders
+  [fact rhs-fn rhs-args path-binders]
   (let [f (subvec fact 1)
-        pattern-binders (mapcat :pattern-binders (:joined-binders rule))
+        pattern-binders (mapcat :pattern-binders (:joined-binders path-binders))
         patterns (->> pattern-binders (map :clause) set)
-        function-binders (:function-binders rule)
-        predicates (:predicates rule)
+        function-binders (:function-binders path-binders)
+        predicates (:predicates path-binders)
         ; TODO - check for conflicts
-        pattern-fact-bindings (mapv (partial bind-patterns f) (map :pattern-binders (:joined-binders rule)))]
-    (println (:name rule) fact pattern-fact-bindings)
+        pattern-fact-bindings (mapv (partial bind-patterns f) (map :pattern-binders (:joined-binders path-binders)))]
+    (println fact pattern-fact-bindings)
     (if (not-empty pattern-fact-bindings)
       (let [op (first fact)
-            bindings-by-join (mapv (fn [jpb pfb] (update-pattern-bindings op jpb pfb)) (:joined-binders rule) pattern-fact-bindings)
-            rule (assoc rule :joined-binders bindings-by-join)
+            bindings-by-join (mapv (fn [jpb pfb] (update-pattern-bindings op jpb pfb)) (:joined-binders path-binders) pattern-fact-bindings)
+            updated-path-binders (assoc path-binders :joined-binders bindings-by-join)
             cross-joins (->> bindings-by-join
                              (map (fn [jb]
                                     (->> jb
@@ -345,35 +413,42 @@
                                       (if b' (conj bs b') bs)))
                                   #{}))
             _ (pprint bindings)
-            existing-bindings (set (-> rule :activations keys))
+            existing-bindings (set (-> updated-path-binders :activations keys))
             _ (pprint existing-bindings)
             new-bindings (set/difference bindings existing-bindings)
             _ (pprint new-bindings)
             retracted-bindings (set/difference existing-bindings bindings)
             _ (pprint retracted-bindings)
-            rule (if (not-empty retracted-bindings)
-                   (let [tx-data (set (->> retracted-bindings
-                                           (mapcat (:activations rule))
-                                           (filter #(not= :db/add! (first %)))
-                                           (map #(assoc % 0 :db/retract))))
-                         activations (apply dissoc (:activations rule) retracted-bindings)]
-                     (-> rule
-                         (assoc :activations activations)
-                         (update :tx-data concat tx-data)))
-                   rule)
-            rule (if (not-empty new-bindings)
-                   (let [activations (->> new-bindings
-                                          (map (juxt identity
-                                                     #(apply (:rhs-fn rule) (map % (:rhs-args rule)))))
-                                          (into {}))
-                         tx-data (set (mapcat val activations))]
-                     (-> rule
-                         (update :activations merge activations)
-                         (update :tx-data concat tx-data)))
-                   rule)]
-        rule)
-      rule)))
+            updated-path-binders (if (not-empty retracted-bindings)
+                                   (let [tx-data (set (->> retracted-bindings
+                                                           (mapcat (:activations updated-path-binders))
+                                                           (filter #(not= :db/add! (first %)))
+                                                           (map #(assoc % 0 :db/retract))))
+                                         activations (apply dissoc (:activations updated-path-binders) retracted-bindings)]
+                                     (-> updated-path-binders
+                                         (assoc :activations activations)
+                                         (update :tx-data concat tx-data)))
+                                   updated-path-binders)
+            updated-path-binders (if (not-empty new-bindings)
+                                   (let [activations (->> new-bindings
+                                                          (map (juxt identity
+                                                                     #(apply rhs-fn (map % rhs-args))))
+                                                          (into {}))
+                                         tx-data (set (mapcat val activations))]
+                                     (-> updated-path-binders
+                                         (update :activations merge activations)
+                                         (update :tx-data concat tx-data)))
+                                   updated-path-binders)]
+        updated-path-binders)
+      path-binders)))
 
+(defn run-rule
+  [fact rule]
+  (println "*********" (:name rule))
+  (let [updated-path-binders (mapv (partial run-path-binders fact (:rhs-fn rule) (:rhs-args rule)) (:path-binders rule))
+        tx-data (reduce into [] (map :tx-data updated-path-binders))]
+    (assoc rule :path-binders updated-path-binders
+                :tx-data tx-data)))
 
 (defn transact1
   [session fact]
