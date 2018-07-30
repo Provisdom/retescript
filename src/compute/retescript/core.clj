0cf20a91875b0a43ebbdffc2becb962ec140b7ff
dave <dave@computesoftware.com>
2018-07-30 16:51:13 -0700

    On second thought, don't filter based on patterns.

--- src/compute/retescript/core.clj
+++ src/compute/retescript/core.clj
@@ -264,7 +264,7 @@
                          #(contains? (:patterns %) fact-pattern)
                          #(not (contains? (:patterns %) fact-pattern)))
         joined-bindings (->> (:bindings joined-pattern-binder)
-                             (filter pattern-filter)
+                             #_(filter pattern-filter)
                              (mapv (fn [%]
                                      (let [binding (:binding %)
                                            join? (reduce (fn [j? fv]
@@ -441,67 +441,67 @@
     [[:db/add ?e :foo :bar]]
     #_(println "R2" ?e ?v)]
 
-   #_[::r3
-      [:find ?e ?x ?z
-       :where
-       [?e :a ?v]
-       [(+ ?x 2) ?z]
-       [(* ?v 0.3) ?x]]
-      =>
-      #_(println "R3" ?x ?z)
-      [[:db/add ?e :x ?x]]]
+   [::r3
+    [:find ?e ?x ?z
+     :where
+     [?e :a ?v]
+     [(+ ?x 2) ?z]
+     [(* ?v 0.3) ?x]]
+    =>
+    #_(println "R3" ?x ?z)
+    [[:db/add ?e :x ?x]]]
 
-   #_[::r4
-      [:find ?e1 ?v2
-       :where
-       [?e1 :a _]
-       [_ :a ?v2]
-       #_[_ :b ?v2]
-       #_[(identity ?v2) ?q]
-       #_[(inc ?v2) ?q]
-       #_[?e1 :a ?q]]
-      =>
-      [[:db/add ?e1 :foo :bar]]
-      #_(println "R4" ?e1 ?v2)]
+   [::r4
+    [:find ?e1 ?v2
+     :where
+     [?e1 :a _]
+     [_ :a ?v2]
+     #_[_ :b ?v2]
+     #_[(identity ?v2) ?q]
+     #_[(inc ?v2) ?q]
+     #_[?e1 :a ?q]]
+    =>
+    [[:db/add ?e1 :foo :bar]]
+    #_(println "R4" ?e1 ?v2)]
 
-   #_[::r5
-      [:find ?e ?v ?w ?q
-       :where
-       [?e :a ?v]
-       [?e :b ?w]
-       [?e :c ?q]
-       (not [?e :c 1]
-            [?e :d 2]
-            [(> ?w 5)]
-            #_[(identity ?e) ?e])
-       (or [?e :a 1]
-           (and [?e :a 2]
-                [?e :b 1]))]
-      =>
-      #_(println "R5" ?e ?v ?w ?q)
-      [[:db/add ?e :foo :bar]]]
+   [::r5
+    [:find ?e ?v ?w ?q
+     :where
+     [?e :a ?v]
+     [?e :b ?w]
+     [?e :c ?q]
+     (not [?e :c 1]
+          [?e :d 2]
+          [(> ?w 5)]
+          #_[(identity ?e) ?e])
+     (or [?e :a 1]
+         (and [?e :a 2]
+              [?e :b 1]))]
+    =>
+    #_(println "R5" ?e ?v ?w ?q)
+    [[:db/add ?e :foo :bar]]]
 
-   #_[::r6
-      [:find ?e
-       :where
-       [?e :b 1]
-       [1 :a 1]]
-      =>
-      (println "R6" ?e)]
+   [::r6
+    [:find ?e
+     :where
+     [?e :b 1]
+     [1 :a 1]]
+    =>
+    (println "R6" ?e)]
 
-   #_[::q1
-      [:find ?e ?v ?w ?q
-       :where
-       [?e :a ?v]
-       [?e :b ?w]
-       [?e :c ?q]
-       (not [?e :c 1]
-            [?e :d 2]
-            [(> ?w 5)]
-            #_[(identity ?e) ?e])
-       (or [?e :a 1]
-           (and [?e :a 2]
-                [?e :b 1]))]]])
+   [::q1
+    [:find ?e ?v ?w ?q
+     :where
+     [?e :a ?v]
+     [?e :b ?w]
+     [?e :c ?q]
+     (not [?e :c 1]
+          [?e :d 2]
+          [(> ?w 5)]
+          #_[(identity ?e) ?e])
+     (or [?e :a 1]
+         (and [?e :a 2]
+              [?e :b 1]))]]])
 
 (def s (create-session rs))
 
